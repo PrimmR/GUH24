11e6b6dbf5fc50e5a3cc3d364357417268e91721
PrimmR <primmr@icloud.com>
2024-11-09 17:43:22 +0000

    Woo cross-fade works (or does it?)

--- main.js
+++ main.js
@@ -4,12 +4,18 @@ years = [
     {
         year: 3,
         description: "Hello",
-        image: "PanoBlueIPad.png"
+        image: "img/PanoBlueIPad.png"
     },
+    // {
+    //     year: 27,
+    //     description: "Goodbye",
+    //     image: "img/PanoPurpIPad.png"
+    // }
+    // , 
     {
-        year: 27,
-        description: "Goodbye",
-        image: "PanoPurpIPad.png"
+        year: 5,
+        description: "Image 5",
+        image: "img/PanoPurpIPad.png"
     }
 ]
 
@@ -22,18 +28,39 @@ function sort_years() {
 
 document.addEventListener("DOMContentLoaded", (event) => {
     let slider = document.getElementById("slidebar");
-    slider.max = years.length;
+    slider.max = years.length - 1;
 
     let output = document.getElementById("demo");
     output.innerHTML = slider.value; // Display the default slider value
 
     sort_years()
 
+    years.forEach(e => {
+        let image = document.createElement("img");
+        image.src = e.image
+        image.id = `bg_${e.year}`
+        image.classList.add("background")
+        document.getElementById("bg").append(image)
+    });
+
 
     // Update the current slider value (each time you drag the slider handle)
     slider.oninput = function () {
-        output.innerHTML = this.value;
+        current_year = years[this.value]
+        output.innerHTML = current_year.description;
 
-        document.getElementById("bg_img2").style.opacity = this.value
+
+        Array.from(document.getElementsByClassName("background")).forEach(e => {
+            e.style.zIndex = -10
+            e.animate({
+                opacity: [1, 0],
+            }, 1000)
+        })
+
+        let top_image = document.getElementById(`bg_${current_year.year}`)
+        top_image.style.zIndex = -9
+        top_image.animate({
+            opacity: [0, 1],
+        }, 1000)
     }
 });
